aa1dbce7e767fa0c1c6bbaf12703f32bdb4e3361
InmookJeong <jeonginmook@gmail.com>
2024-04-16 23:49:21 +0900

    refactor - Update fields of UserResultDTO
    Update fields and methods of UserResultDTO.
    Update comments in UserResultDTO.

--- user/src/main/java/kr/mook/user/common/dto/UserResultDTO.java
+++ user/src/main/java/kr/mook/user/common/dto/UserResultDTO.java
@@ -1,11 +1,17 @@
 package kr.mook.user.common.dto;
 
+import java.util.HashMap;
+
 /**
  * UserResultDTO is an object that returns results such as login, logout, and membership registration.<br/>
  * Fields<br/>
- * &emsp;- status : Query Result Status field.<br/>
- * &emsp;- message : Query result message field.<br/>
- * &emsp;- data : Query result data field.
+ * &emsp;- apiTitle : API title field.<br/>
+ * &emsp;- status : Query result status code field.<br/>
+ * &emsp;- statusMessage : Query result message field.<br/>
+ * &emsp;&emsp;- statusMessage Example : {eng : "English Message", kor : "한글 메시지"}<br/>
+ * &emsp;- contentType : Query result data type field.<br/>
+ * &emsp;&emsp;- Depending on the data format stored in the content, Number, String, Boolean, Array, Object, etc. are input.<br/>
+ * &emsp;- content : Query result data field.
  * 
  * @since 2024.03.11
  * @author In-mook, Jeong
@@ -14,47 +20,65 @@ package kr.mook.user.common.dto;
 public class UserResultDTO {
 	
 	// Fields
+	private String apiTitle;
 	private String status;
-	private String message;
-	private String data;
+	private HashMap<String, String> statusMessage;
+	
+	private String contentType;
+	private Object content;
 	
 	// Default Constructor
 	public UserResultDTO() {}
 
 	// Constructor
-	public UserResultDTO(String status, String message, String data) {
-		this.status = status;
-		this.message = message;
-		this.data = data;
+	public UserResultDTO(String apiTitle) {
+		this.apiTitle = apiTitle;
 	}
 
 	// Getters and Setters
+	public String getApiTitle() {
+		return apiTitle;
+	}
+
 	public String getStatus() {
 		return status;
 	}
 
-	public void setStatus(String status) {
+	public HashMap<String, String> getStatusMessage() {
+		return statusMessage;
+	}
+
+	public void setStatus(String status, String engStatusMessage, String korStatusMessage) {
+		// setStatus
 		this.status = status;
+		
+		// setStatusMessage
+		HashMap<String, String> statusMessage = new HashMap<String, String>();
+		statusMessage.put("eng", engStatusMessage);
+		statusMessage.put("kor", korStatusMessage);
+		this.statusMessage = statusMessage;
 	}
 
-	public String getMessage() {
-		return message;
+	public String getContentType() {
+		return contentType;
 	}
 
-	public void setMessage(String message) {
-		this.message = message;
+	public Object getContent() {
+		return content;
 	}
-	
-	public String getData() {
-		return data;
-	}
-	
-	public void setData(String data) {
-		this.data = data;
+
+	public void setContent(String contentType, Object content) {
+		// setContentType
+		this.contentType = contentType;
+		this.content = content;
 	}
 
 	@Override
 	public String toString() {
-		return "UserResourceResultDTO [status=" + status + ", message=" + message + ", data=" + data + "]";
+		return "UserResultDTO [apiTitle=" + apiTitle
+				+ ", status=" + status 
+				+ ", statusMessage={eng:" + statusMessage.get("eng") + ", kor:" + statusMessage.get("kor") + "}"
+				+ ", contentType=" + contentType 
+				+ ", content=" + content + "]";
 	}
 }
